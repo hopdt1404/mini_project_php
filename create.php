9c90480a1a865a1c3cc7742ca78b82d87d1382bc
hopdt <hopdt@ghtk.vn>
2020-06-24 10:27:45 +0700

    done create method. Still show alert

--- create.php
+++ create.php
@@ -1,3 +1,56 @@
+<?php
+// Include config file
+require_once "config.php";
+
+// Define variables and initialize with empty values
+$name = $address = $salary = "";
+$name_err = $address_err = $salary_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $name = $input_name;
+    }
+
+    // Validate address
+    $input_address = trim($_POST["address"]);
+    if(empty($input_address)){
+        $address_err = "Please enter an address.";
+    } else{
+        $address = $input_address;
+    }
+
+    // Validate salary
+    $input_salary = trim($_POST["salary"]);
+    if(empty($input_salary)){
+        $salary_err = "Please enter the salary amount.";
+    } elseif(!ctype_digit($input_salary)){
+        $salary_err = "Please enter a positive integer value.";
+    } else{
+        $salary = $input_salary;
+    }
+
+    // Check input errors before inserting in database
+    if(empty($name_err) && empty($address_err) && empty($salary_err)) {
+        $sql = "INSERT INTO employees (name, address, salary) VALUES (\"" . $name . "\", \"" .  $address . "\", " . $salary . ");";
+        $result = $conn->query($sql);
+        if ($result) {
+            $message = '<div class="alert alert-success" role="alert">Success</div>';
+            header("location: home.php");
+            exit();
+        } else {
+            echo "Something went wrong. Please try again later.";
+        }
+    }
+}
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
